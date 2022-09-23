38eae322a5db2f6f785b98aa6d8c3d44aba8b642
Gregtmj <greg_1461996@hotmail.com>
2022-09-23 17:33:05 +0300

    brain_calcadded

--- Makefile
+++ Makefile
@@ -8,13 +8,16 @@ publish:
 	poetry publish --dry-run
 
 package-install:
-	python -m pip install  dist/hexlet_code-0.1.0-py3-none-any.whl
+	python -m pip install --force-reinstall dist/hexlet_code-0.1.0-py3-none-any.whl
 
 brain-games:
 	poetry run brain-games
 
 brain-even:
 	poetry run brain-even
+
+brain-calc:
+	poetry run brain-calc
 
 lint:
 	poetry run flake8 brain_games
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,49 @@
+"""
+Функционал игры "Арифметические вычисления"
+"""
+from random import randint, choice
+from .cli import welcome_user
+
+
+SIMPLE_ARITHMETIC_SYMBOLS = ['+', '-', '/', '*']
+
+
+def brain_calc(name: str) -> None:
+    """
+    Функция для вычисления арифметических задач
+    """
+    print("What is the result of the expression?")
+    player_result: int = 0  # Счётчик для ослеживания результата игрока (необ.)
+    tries_count: int = 0  # Счётчик для отслеживания количества попыток
+    while tries_count != 3:
+        first_number: int = randint(1, 10)
+        second_number: int = randint(1, 10)
+        operator: str = choice(SIMPLE_ARITHMETIC_SYMBOLS)
+        tries_count += 1  # Прибавляем количество ходов
+        answer_evaluation: float = eval(f"{first_number} {operator}"
+                                        f" {second_number}")
+        print(f"Question: {first_number} {operator} {second_number}")
+        player_answer: float = float(input("Your answer: "))
+        # Иногда получаем дробные числа, поэтому округляем на 2 цифры
+        if player_answer == round(answer_evaluation, 2):
+            player_result += 1
+            print("Correct!")
+        else:
+            print(f"{player_answer} is wrong answer ;(. Correct "
+                  f"answer is {round(answer_evaluation,2)}")
+            print(f"Let's try again, {name}!")
+            break
+    if player_result == 3:
+        print(f"Congratulations, {name}!")
+
+
+def main() -> None:
+    """
+    Функция для тестирование
+    """
+    player_name: str = welcome_user()
+    brain_calc(player_name)
+
+
+if __name__ == '__main__':
+    main()
